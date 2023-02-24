f9a9210def55136c38ec11803b1c37c022bd92ca
miirsery <miirsery.main@gmail.com>
2023-02-24 13:31:44 +0700

    add template layout

--- src/app/components/layouts/BaseLayout/BaseLayout.vue
+++ src/app/components/layouts/BaseLayout/BaseLayout.vue
@@ -1,11 +1,71 @@
 <template>
   <div class="base-layout">
-    <base-header />
+    <base-sidebar />
+    <div class="base-layout__content-wrapper">
+      <base-header />
 
-    <router-view />
+      <div class="base-layout__content">
+        <div class="base-layout__container">
+          <router-view />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import BaseHeader from '@/widgets/BaseLayout/BaseHeader/BaseHeader.vue'
+import BaseSidebar from '@/widgets/BaseLayout/BaseSidebar/BaseSidebar.vue'
 </script>
+
+<style lang="scss" scoped>
+.base-layout {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+
+  &__content-wrapper {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @include responsive(xs) {
+      height: calc(100% - 60px);
+    }
+  }
+
+  &__content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 50px;
+    overflow-y: auto;
+
+    @include responsive(xs, $breakpoints-only-max) {
+      padding: 0 10px 40px;
+    }
+  }
+
+  &__container {
+    width: 100%;
+    height: fit-content;
+    max-width: 1016px;
+
+    @include responsive(sm, $breakpoints-only-max) {
+      max-width: 620px;
+    }
+
+    @include responsive(xs, $breakpoints-only-max) {
+      max-width: 355px;
+    }
+  }
+
+  @include responsive(xs) {
+    flex-direction: column-reverse;
+  }
+}
+</style>
---
+++ src/widgets/BaseLayout/BaseSidebar/BaseSidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="base-sidebar">
+    <aside>
+      <div>Главная</div>
+      <div>Тудухи</div>
+    </aside>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.base-sidebar {
+  width: 350px;
+  box-shadow: 0 2px 16px $color--primary;
+  padding-right: 20px;
+}
+</style>
